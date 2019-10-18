3f2facc2c233ec49505487d2b0fa6ba91d7e4a33
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-18 10:35:43 -0700

    added resource directory

---
+++ resource/neighborhoods.py
@@ -0,0 +1,24 @@
+import json
+from pprint import pprint
+
+"""
+All information in geojson file is under 'features' key
+'properties' key under 'name' gives name of district
+'geometry' key under 'coordinates' contains list of lon and lat coordinates
+
+"""
+
+
+data = "SF_FIND_Neighborhoods.geojson"
+districts = open(data).read()
+
+districts_data = json.loads(districts)
+
+
+district_name = districts_data['features'][0]['properties']['name']
+district_coordinates = districts_data['features'][0]['geometry']['coordinates'][0][0]
+
+
+# allows you to view the districts of SF
+for i,district in enumerate(districts_data['features']):
+    pprint(districts_data['features'][i]['properties']['name'])
