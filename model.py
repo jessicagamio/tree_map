77f46e1ec8f136789418576030fd56e6ea661ee3
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-21 12:53:23 -0700

    practiced SQLAlchemy setup

--- model.py
+++ model.py
@@ -1,23 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
-db = SQLAlchemy()
+def connect_to_db(app,dbname='tree_map'):
+    """connect to database"""
+    db.config['SQLALCHEMY_DATABASE_URI']=f'postgres:///{dbname}'
+    db.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False 
+    db.config['SQLALCHEMY_ECHO'] = True
 
-def connect_to_db(app,db_name):
-    """Connects to database"""
-
-    app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql:///{db_name}'
-    #shows SQL executed
-    app.config['SQLALCHEMY_ECHO']= True
-    app.config['SQLAlCHEMY_TRACK_MODIFICATIONS']= False
     db.app = app
-    db_init_app(app)
-    db.create_all()
+    db.init_app(app)
+    db.connect_all()
 
-    print('Connected to db!')
+
 
 
 
 
 
 if __name__=="__main__":
+    from server import app
     connect_to_db(app)
