646094fd4de925506a59c994c4f6701b56437988
jessicagamio <jessica.gamio@gmail.com>
2019-12-01 13:46:05 -0800

    created function to return district point belongs to

--- resource/shapelytest.py
+++ resource/shapelytest.py
@@ -4,19 +4,19 @@ import json
 
 
 
-with open('SF_Find_Neighborhoods.geojson') as f:
-    js = json.load(f)
+def findDistrict(lat,lon)
 
+    with open('SF_Find_Neighborhoods.geojson') as f:
+        js = json.load(f)
 
-# point= Point(-122.457133,37.737350) # in sherwood
-# point=Point(-122.462396,37.733606) # not in montery heights
-# point=Point(-122.462396,37.733606)
-point=Point(-122.49024,37.761847)#outer sunset
+    point=Point(lon,lat)#outer sunset
 
-for feature in js['features']:
-    polygon = shape(feature['geometry'])
-    if polygon.contains(point):
-        print('found in polygon',feature['properties']['name'])
+    for feature in js['features']:
+        polygon = shape(feature['geometry'])
+        if polygon.contains(point):
+            district = feature['properties']['name']
+
+    return district
 
 
 
