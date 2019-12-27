0b3b279887a07246f4a176b7cc356fe548cc3eb9
jessicagamio <jessica.gamio@gmail.com>
2019-12-27 15:29:20 -0800

    working on debugging keyerror=None on line 86 issue

--- seed.py
+++ seed.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from model import db, TreeSpecies, Locations, Districts, connect_to_db
 from shapely.geometry import Point,Polygon,shape
@@ -10,6 +11,7 @@ if __name__ == "__main__":
 
 def findDistrict(lat,lon):
     """returns district the point/tree belongs to"""
+    # district = ''
 
     with open('resource/SF_Find_Neighborhoods.geojson') as f:
         js = json.load(f)
@@ -21,7 +23,7 @@ def findDistrict(lat,lon):
         if polygon.contains(point):
             district = feature['properties']['name']
 
-    return district
+            return district
 
 # Record all Neighborhoods in Districts db
 data = "resource/SF_FIND_Neighborhoods.geojson"
@@ -29,12 +31,17 @@ districts = open(data).read()
 
 districts_data = json.loads(districts)
 
+district_dict={}
+
 for i,district in enumerate(districts_data['features']):
     district_name = districts_data['features'][i]['properties']['name']
     coord = districts_data['features'][i]['geometry']['coordinates'][0][0]
 
     # Add districts to db
     tree_district=Districts(district_name= district_name, coord = coord)
+
+    district_dict[tree_district.district_name] = tree_district
+
     db.session.add(tree_district)
     db.session.commit()
 
@@ -46,6 +53,8 @@ tree_data_json = open(tree_data).read()
 street_trees = json.loads(tree_data_json)
 entries = street_trees['data'].__len__()
 
+print('*********number of entries*******', entries)
+
 # iterated through all json file and record tree names and location coordinates
 while (i<entries):
     tree_type =street_trees['data'][i][10]
@@ -53,7 +62,7 @@ while (i<entries):
     longitude = street_trees['data'][i][24]
 
 
-    if latitude == None and longitude==None:
+    if latitude == None or longitude==None:
         pass
     
     elif tree_type == 'Tree(s) ::':
@@ -63,21 +72,28 @@ while (i<entries):
         sci_name, common_name = tree_type.split('::')
         sci_name = sci_name.rstrip()
         common_name = common_name.lstrip()  
-    # add Tree Specias to db
-    tree_type = TreeSpecies(sci_name=sci_name, common_name=common_name)
-    db.session.add(tree_type)
+    
+    # if latitude != None and longitude != None:
+        # add Tree Specias to db
+        tree_type = TreeSpecies(sci_name=sci_name, common_name=common_name)
+        db.session.add(tree_type)
 
-    # find district tree species is located in
-    districtName = findDistrict(float(latitude), float(longitude))
+        # find district tree species is located in
+        districtName = findDistrict(float(latitude), float(longitude))
 
-    # fetch district from Districts class before passing to relationship
-    districtIn = db.session.query(Districts).filter_by(district_name=districtName).first()
+   
+        print('------district info-------->',districtName)
 
-    # record tree Locations
-    tree_loc = Locations(lat= float(latitude), lon= float(longitude), tree_species= tree_type, districts= districtIn)
-    db.session.add(tree_loc)
+        # fetch district from Districts class before passing to relationship
+        districtIn = district_dict[districtName]
 
-    db.session.commit()
+        print('----name----->',districtIn)
+
+        # record tree Locations
+        tree_loc = Locations(lat= float(latitude), lon= float(longitude), tree_species= tree_type, districts= districtIn)
+        db.session.add(tree_loc)
+
+        db.session.commit()
 
     i+=1
 
